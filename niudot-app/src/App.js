a921f98c479f143a2e31d8ab7eb7050b4cb84210
Carlos Castillo <carloseduardocp.01@gmail.com>
2020-10-03 00:24:12 -0300

    Implementing hover background, will change to color

--- niudot-app/src/App.js
+++ niudot-app/src/App.js
@@ -3,25 +3,22 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Sidebar from './components/layout/sidebar/Sidebar'
 import Login from './components/login/Login'
 import Page from './components/pages/Page'
-import SidebarState from './context/sidebar/SidebarState'
 
 function App() {
 	return (
-		<SidebarState>
-			<Router>
-				<Switch>
-					<Route exact path='/' component={Login} />
-					<Route
-						render={(props) => (
-							<>
-								<Sidebar />
-								<Page />
-							</>
-						)}
-					/>
-				</Switch>
-			</Router>
-		</SidebarState>
+		<Router>
+			<Switch>
+				<Route exact path='/' component={Login} />
+				<Route
+					render={(props) => (
+						<>
+							<Sidebar />
+							<Page />
+						</>
+					)}
+				/>
+			</Switch>
+		</Router>
 	)
 }
 
--- niudot-app/src/components/layout/sidebar/Sidebar.js
+++ niudot-app/src/components/layout/sidebar/Sidebar.js
@@ -1,53 +1,58 @@
-import React, { useState, useEffect, useContext } from 'react'
-import SidebarItem from './SidebarItem'
-import pages from '../../pages/pages'
-import { FaHome } from 'react-icons/fa/index'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import { sidebarContext } from '../../../context/sidebar/SidebarState'
+import {
+	ProSidebar,
+	SubMenu,
+	SidebarContent,
+	SidebarHeader,
+	Menu,
+	MenuItem
+} from 'react-pro-sidebar'
+import {
+	FaHome,
+	FaCashRegister,
+	FaUsers,
+	FaPiggyBank,
+	FaCreditCard,
+	FaChartBar,
+	FaReceipt,
+	FaCircle,
+	FaChevronCircleDown
+} from 'react-icons/fa/index'
 
 function Sidebar() {
-	const [activedDropdown, setActiveDropdown] = useState('')
-	const { showSidebar, setShowSidebar } = useContext(sidebarContext)
-
-	useEffect(() => {
-		if (window.innerWidth >= 576) {
-			setShowSidebar(true)
-		}
-	}, [setShowSidebar])
-
-	function toggleDropwdown(dropdownToOpen) {
-		if (dropdownToOpen !== 'Inicio') {
-			setActiveDropdown(dropdownToOpen)
-		}
-	}
-
 	return (
-		<div className={`sidebar-container ${showSidebar || 'sidebar-hide'}`}>
-			<h2 className='sidebar-logo'>niudot.</h2>
-			<nav className='sidebar-nav'>
-				<ul>
-					<Link to='/inicio' onClick={() => setActiveDropdown('inicio')}>
-						<li className='sidebar-list-item rounded'>
-							<div className='sidebar-list-item-info'>
-								<FaHome className='sidebar-item-icon' />
-								Inicio
-							</div>
-						</li>
-					</Link>
-					{pages.map((page) => (
-						<SidebarItem
-							key={page.name}
-							name={page.name}
-							iconCode={page.iconCode}
-							dropdownOptions={page.dropdownOptions}
-							isDropdownActive={activedDropdown === page.name ? true : false}
-							toggleDropdown={toggleDropwdown}
-						/>
-					))}
-				</ul>
-			</nav>
-		</div>
+		<ProSidebar>
+			<SidebarHeader>
+				<h2>niudot.</h2>
+			</SidebarHeader>
+			<SidebarContent className='sidebar-nav'>
+				<Menu>
+					<MenuItem icon={<FaHome />} className='sidebar-list-item rounded'>
+						Inicio
+					</MenuItem>
+					<SubMenu
+						title='Caja'
+						icon={<FaCashRegister />}
+						className='sidebar-list-item rounded'
+					>
+						<MenuItem>1</MenuItem>
+						<MenuItem>2</MenuItem>
+						<MenuItem>3</MenuItem>
+					</SubMenu>
+				</Menu>
+			</SidebarContent>
+		</ProSidebar>
 	)
+}
+
+{
+	/* <li className='sidebar-list-item rounded'>
+								<div className='sidebar-list-item-info'>
+									<FaHome className='sidebar-item-icon' />
+									Inicio
+								</div>
+							</li> */
 }
 
 export default Sidebar
