f1016899b15a1620ed728c90b26e5887b0808e2d
Carlos Castillo <carloseduardocp.01@gmail.com>
2020-10-03 00:47:53 -0300

    Perdi 3 horas de mi vida

--- niudot-app/src/components/layout/sidebar/Sidebar.js
+++ niudot-app/src/components/layout/sidebar/Sidebar.js
@@ -1,14 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {
-	ProSidebar,
-	SubMenu,
-	SidebarContent,
-	SidebarHeader,
-	Menu,
-	MenuItem
-} from 'react-pro-sidebar'
-import {
 	FaHome,
 	FaCashRegister,
 	FaUsers,
@@ -22,37 +14,8 @@ import {
 
 function Sidebar() {
 	return (
-		<ProSidebar>
-			<SidebarHeader>
-				<h2>niudot.</h2>
-			</SidebarHeader>
-			<SidebarContent className='sidebar-nav'>
-				<Menu>
-					<MenuItem icon={<FaHome />} className='sidebar-list-item rounded'>
-						Inicio
-					</MenuItem>
-					<SubMenu
-						title='Caja'
-						icon={<FaCashRegister />}
-						className='sidebar-list-item rounded'
-					>
-						<MenuItem>1</MenuItem>
-						<MenuItem>2</MenuItem>
-						<MenuItem>3</MenuItem>
-					</SubMenu>
-				</Menu>
-			</SidebarContent>
-		</ProSidebar>
+		
 	)
-}
-
-{
-	/* <li className='sidebar-list-item rounded'>
-								<div className='sidebar-list-item-info'>
-									<FaHome className='sidebar-item-icon' />
-									Inicio
-								</div>
-							</li> */
 }
 
 export default Sidebar
