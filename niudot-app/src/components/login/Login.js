1d190da703ceeab488a7bbdf397d3331af510a37
satbh <carlosarcia1827@gmail.com>
2020-11-13 09:31:54 -0600

    Cambios en el login y css

--- niudot-app/src/components/login/Login.js
+++ niudot-app/src/components/login/Login.js
@@ -7,50 +7,57 @@ import authContext from '../../context/auth/authContext'
 function Login() {
 	const {logUser} = useContext(authContext)
 
+	const initialValues = {
+		username: '',
+		password: ''
+	}
+
+	const onSubmit = async (values) => {
+		try {
+			const req = await fetch('https://nicascriptproject.uc.r.appspot.com/auth', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Credentials': 'true'
+				},
+				mode:'cors',
+				body: JSON.stringify(values),
+			})
+			const data = await req.text()
+			console.log(data)
+
+		} catch (err) {
+			console.error(err.message)
+		}
+	}
+
 	return (
-		<Formik
-			initialValues={{
-				username: '',
-				password: ''
-			}}
-			onSubmit={async (values) => {
-				try {
-					const req = await fetch('http://34.68.232.139:5000/login', {
-						method: 'POST',
-						headers: {
-							'Content-Type': 'application/json'
-						},
-						body: JSON.stringify(values),
-					})
-					const data = await req.json()
-
-					console.log(data)
-					logUser(data['user_token'])
-
-				} catch (err) {
-					console.error(err.message)
-				}
-			}}
-		>
-			<Form>
-				<div className='form'>
-					<div className='form-container'>
-						<FormTextInput
-							name='username'
-							classes='lg'
-							placeholder='Enter username'
-						/>
-						<FormTextInput
-							name='password'
-							classes='lg'
-							placeholder='Enter password'
-							type='password'
-						/>
-						<button type='submit'>Submit</button>
-					</div>
-				</div>
-			</Form>
-		</Formik>
+		<div className='login-container'>
+			<div style={{display:'flex', justifyContent: 'center'}}>
+				<Formik initialValues={initialValues} onSubmit={onSubmit}>
+					<Form>
+						<div className='form'>
+							<div className='form-container vlogin-container'>
+								<FormTextInput
+									name='username'
+									classes='lg'
+									placeholder='Enter username'
+									label='Username'
+								/>
+								<FormTextInput
+									name='password'
+									label='Password'
+									classes='lg'
+									placeholder='Enter password'
+									type='password'
+								/>
+								<button type='submit' style={{height:"30px"}}>Submit</button>
+							</div>
+						</div>
+					</Form>
+				</Formik>
+			</div>
+		</div>
 	)
 }
 
