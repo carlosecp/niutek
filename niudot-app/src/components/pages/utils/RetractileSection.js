8263a8fc3d6abd2959221fcf4d2ba714d2c90b03
satbh <carlosarcia1827@gmail.com>
2020-11-29 05:25:01 -0600

    Cambio a las secciones retractiles para consumir el contexto

--- niudot-app/src/components/pages/utils/RetractileSection.js
+++ niudot-app/src/components/pages/utils/RetractileSection.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React , {useContext} from 'react'
+import {PageContext} from '../../../context/sections/SectionsContext'
 import RetractileFormTemplate from './RetractileFormTemplate'
 
 
 /* Seccion retractil de una pagina. Toma el estado de la pagina, definido como
 un objeto que indexa el estado de cada seccion segun el titulo.*/
-export default function RetractileSection({pageState, formTitle, children}){
-	const retractionHook = pageState[formTitle]
+export default function RetractileSection({formTitle, children}){
+	const {sections} = useContext(PageContext) 
+	const retractionHook = sections[formTitle]
+
 	const props = {...retractionHook, formTitle, children}
 
 	return <RetractileFormTemplate {...props}/>
