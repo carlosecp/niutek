79f53ae1dfd4ce51b6df0e05fc21e42ea411af7e
satbh <carlosarcia1827@gmail.com>
2020-12-03 04:09:38 -0600

    Inicializacion del context de la pagina de persona natural

--- niudot-app/src/components/pages/clientes/personanatural/PersonaNatural.js
+++ niudot-app/src/components/pages/clientes/personanatural/PersonaNatural.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useContext, useEffect} from 'react'
 
 import PageMenu from '../../utils/PageMenu'
 import SearchUserForm from '../../utils/SearchUserForm'
@@ -6,7 +6,7 @@ import SearchUserForm from '../../utils/SearchUserForm'
 import CreateNewClientBtn from '../../utils/CreateNewClientBtn'
 import PersonaNaturalCreate from './PersonaNaturalCreate'
 import { Link } from 'react-router-dom'
-
+import {PageContext} from '../../../../context/sections/SectionsContext'
 // * Comentarios para Juan
 {
 	/* Esta es la pantalla principal de Persona Natural, aca no hay muchas cosas interesantes la verdad. Acuerdense de que 	ustedes van a cambiar algo aca porque tenemos el menu de la derecha... Pero de eso ya no se yo... Asi que ahora podes avanzar a PersonaNaturalCreate...
@@ -19,13 +19,35 @@ function PersonaNatural() {
 		'datos-profesionales-y-economicos'
 	)
 
-	const sections = [
+	const {sections, setSections} = useContext(PageContext)
+	const PNsections = [
 		'datos-profesionales-y-economicos',
 		'referencias',
 		'comerciales',
 		'bancarias',
 		'personales'
 	]
+
+	const useSection = parent => {
+		const [state, setState] = useState(true)
+		if (parent) {
+			const setChildState = (retracted) => {
+				const [...setParentState] = sections[parent]
+				if(!retracted){
+					setParentState(retracted)
+				}
+				setChildState(retracted)
+			}
+			return [state, setChildState]
+		}
+		return [state, setState]
+	}
+
+
+	useEffect(() => setSections(PNsections), [])
+	//setSections(PNsections)
+	//console.log('sections:', sections)
+	
 
 	return (
 		<>
