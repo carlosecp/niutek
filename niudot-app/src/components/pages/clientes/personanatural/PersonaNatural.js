b5f8497d33a1f47cb3873c98cabe0103d7524172
satbh <carlosarcia1827@gmail.com>
2020-12-03 04:23:08 -0600

    fixed merge error

--- niudot-app/src/components/pages/clientes/personanatural/PersonaNatural.js
+++ niudot-app/src/components/pages/clientes/personanatural/PersonaNatural.js
@@ -5,12 +5,9 @@ import SearchUserForm from '../../utils/SearchUserForm'
 
 import CreateNewClientBtn from '../../utils/CreateNewClientBtn'
 import PersonaNaturalCreate from './PersonaNaturalCreate'
-<<<<<<< HEAD
 import { Link } from 'react-router-dom'
 import {PageContext} from '../../../../context/sections/SectionsContext'
-=======
 
->>>>>>> ee87ea2f46cfee87ed64fa9549c3fdde5b38098c
 // * Comentarios para Juan
 {
 	/* Esta es la pantalla principal de Persona Natural, aca no hay muchas cosas interesantes la verdad. Acuerdense de que 	ustedes van a cambiar algo aca porque tenemos el menu de la derecha... Pero de eso ya no se yo... Asi que ahora podes avanzar a PersonaNaturalCreate...
