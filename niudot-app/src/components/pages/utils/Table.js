847a625ea523088d2745bf4a04bf812c906206a2
Carlos Castillo <carloseduardocp.01@gmail.com>
2020-12-13 17:29:13 -0300

    Removed unused i in table file

--- niudot-app/src/components/pages/utils/Table.js
+++ niudot-app/src/components/pages/utils/Table.js
@@ -6,87 +6,86 @@ const EditableCell = ({
 	value: initialValue,
 	row: { index },
 	column: { id },
-	updateMyData, // This is a custom function that we supplied to our table instance
-  }) => {
+	updateMyData // This is a custom function that we supplied to our table instance
+}) => {
 	// We need to keep and update the state of the cell normally
 	const [value, setValue] = React.useState(initialValue)
-  
-	const onChange = e => {
-	  setValue(e.target.value)
+
+	const onChange = (e) => {
+		setValue(e.target.value)
 	}
-  
+
 	// We'll only update the external data when the input is blurred
 	const onBlur = () => {
-	  updateMyData(index, id, value)
+		updateMyData(index, id, value)
 	}
-  
+
 	// If the initialValue is changed external, sync it up with our state
 	React.useEffect(() => {
-	  setValue(initialValue)
+		setValue(initialValue)
 	}, [initialValue])
-  
+
 	return <input value={value} onChange={onChange} onBlur={onBlur} />
-  }
-  
-  // Set our editable cell renderer as the default Cell renderer
-  const defaultColumn = {
-	Cell: EditableCell,
-  }
-  
-
-
-
-export default function Table({ columns, data, updateMyData, skipPageReset }) {
-
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    prepareRow,
-    page,
-  } = useTable(
-    {
-      columns,
-      data,
-      defaultColumn,
-      // use the skipPageReset option to disable page resetting temporarily
-      autoResetPage: !skipPageReset,
-      // updateMyData isn't part of the API, but
-      // anything we put into these options will
-      // automatically be available on the instance.
-      // That way we can call this function from our
-      // cell renderer!
-      updateMyData,
-    },
-    usePagination
-  )
-
-  // Render the UI for your table
-  return (
-    <>
-      <table className='table' {...getTableProps()}>
-        <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {page.map((row, i) => {
-            prepareRow(row)
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                })}
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-    </>
-  )
 }
+
+// Set our editable cell renderer as the default Cell renderer
+const defaultColumn = {
+	Cell: EditableCell
+}
+
+function Table({ columns, data, updateMyData, skipPageReset }) {
+	const {
+		getTableProps,
+		getTableBodyProps,
+		headerGroups,
+		prepareRow,
+		page
+	} = useTable(
+		{
+			columns,
+			data,
+			defaultColumn,
+			// use the skipPageReset option to disable page resetting temporarily
+			autoResetPage: !skipPageReset,
+			// updateMyData isn't part of the API, but
+			// anything we put into these options will
+			// automatically be available on the instance.
+			// That way we can call this function from our
+			// cell renderer!
+			updateMyData
+		},
+		usePagination
+	)
+
+	// Render the UI for your table
+	return (
+		<>
+			<table className='table' {...getTableProps()}>
+				<thead>
+					{headerGroups.map((headerGroup) => (
+						<tr {...headerGroup.getHeaderGroupProps()}>
+							{headerGroup.headers.map((column) => (
+								<th {...column.getHeaderProps()}>{column.render('Header')}</th>
+							))}
+						</tr>
+					))}
+				</thead>
+				<tbody {...getTableBodyProps()}>
+					{page.map((row) => {
+						prepareRow(row)
+						return (
+							<tr {...row.getRowProps()}>
+								{row.cells.map((cell) => {
+									return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+								})}
+							</tr>
+						)
+					})}
+				</tbody>
+			</table>
+		</>
+	)
+}
+
+export default Table
+
