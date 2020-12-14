f856f753bcc5a2130c43c8808c42c3157b12302f
Carlos Castillo <carloseduardocp.01@gmail.com>
2020-12-14 14:34:48 -0300

    Fixed input line break issue

--- niudot-app/src/components/pages/utils/Table.js
+++ niudot-app/src/components/pages/utils/Table.js
@@ -3,62 +3,59 @@ import { useTable, usePagination } from 'react-table'
 import '../../../styles/tables.css'
 import styled from 'styled-components'
 
-
 /* Table Styles */
 
-const Styles = styled.div
+const Styles = styled.div`
+	/* This is required to make the table full-width */
+
+	display: block;
+	max-width: 100%;
+
+	/* This will make the table scrollable when it gets too small */
+
+	.tableWrap {
+		display: block;
+		max-width: 100%;
+		overflow-x: scroll;
+		overflow-y: hidden;
+	}
+
+	table {
+		/* Make sure the inner table is always as wide as needed */
+		width: 100%;
+		border-spacing: 0;
+
+		tr {
+			:last-child {
+				td {
+					border-bottom: 0;
+				}
+			}
+		}
+
+		th,
+		td {
+			margin: 0;
+			padding: 0.5rem;
+
+			/* The secret sauce */
+			/* Each cell should grow equally */
+			width: 1%;
+			/* But "collapsed" cells should be as small as possible */
+			&.collapse {
+				width: 0.0000000001%;
+			}
+
+			:last-child {
+				border-right: 0;
+			}
+		}
+	}
+
+	.pagination {
+		padding: 0.5rem;
+	}
 `
-  /* This is required to make the table full-width */
-
-  display: block;
-  max-width: 100%;
-
-  /* This will make the table scrollable when it gets too small */
-
-  .tableWrap {
-    display: block;
-    max-width: 100%;
-    overflow-x: scroll;
-    overflow-y: hidden;
-  }
-
-  table {
-    /* Make sure the inner table is always as wide as needed */
-    width: 100%;
-    border-spacing: 0;
-
-    tr {
-      :last-child {
-        td {
-          border-bottom: 0;
-        }
-      }
-    }
-
-    th,
-    td {
-      margin: 0;
-      padding: 0.5rem;
-
-      /* The secret sauce */
-      /* Each cell should grow equally */
-      width: 1%;
-      /* But "collapsed" cells should be as small as possible */
-      &.collapse {
-        width: 0.0000000001%;
-      }
-
-      :last-child {
-        border-right: 0;
-      }
-    }
-  }
-
-  .pagination {
-    padding: 0.5rem;
-  }
-`
-
 
 const EditableCell = ({
 	value: initialValue,
@@ -122,35 +119,38 @@ function Table({ columns, data, updateMyData, skipPageReset }) {
 		usePagination
 	)
 
-	
 	// Render the UI for your table
 	return (
 		<Styles>
-		<div className="tableWrap">
-			<table className='table' {...getTableProps()}>
-				<thead>
-					{headerGroups.map((headerGroup) => (
-						<tr {...headerGroup.getHeaderGroupProps()}>
-							{headerGroup.headers.map((column) => (
-								<th {...column.getHeaderProps()}>{column.render('Header')}</th>
-							))}
-						</tr>
-					))}
-				</thead>
-				<tbody {...getTableBodyProps()}>
-					{page.map((row) => {
-						prepareRow(row)
-						return (
-							<tr {...row.getRowProps()}>
-								{row.cells.map((cell) => {
-									return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-								})}
+			<div className='tableWrap'>
+				<table className='table' {...getTableProps()}>
+					<thead>
+						{headerGroups.map((headerGroup) => (
+							<tr {...headerGroup.getHeaderGroupProps()}>
+								{headerGroup.headers.map((column) => (
+									<th {...column.getHeaderProps()}>
+										{column.render('Header')}
+									</th>
+								))}
 							</tr>
-						)
-					})}
-				</tbody>
-			</table>
-		</div>
+						))}
+					</thead>
+					<tbody {...getTableBodyProps()}>
+						{page.map((row) => {
+							prepareRow(row)
+							return (
+								<tr {...row.getRowProps()}>
+									{row.cells.map((cell) => {
+										return (
+											<td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+										)
+									})}
+								</tr>
+							)
+						})}
+					</tbody>
+				</table>
+			</div>
 		</Styles>
 	)
 }
