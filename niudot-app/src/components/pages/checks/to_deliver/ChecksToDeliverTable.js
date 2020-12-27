9eb0c2bb940f2d4d2d0f735bb2f119f3af3af172
Juan Matus <juanmatusg16@gmail.com>
2020-12-27 13:59:16 -0600

    avance

--- niudot-app/src/components/pages/checks/to_deliver/ChecksToDeliverTable.js
+++ niudot-app/src/components/pages/checks/to_deliver/ChecksToDeliverTable.js
@@ -1,198 +1,51 @@
 // React and Router Stuff
-import React, { useState } from 'react'
-// Extra libraries
-import * as Yup from 'yup'
-import { Formik } from 'formik'
-import DatePicker, { registerLocale, setDefaultLocale } from 'react-datepicker'
-import { FaPaperPlane, FaPrint } from 'react-icons/fa'
-import Popup from 'reactjs-popup'
-import Table from '../../utils/Table'
+import React from 'react'
 // Other Components
-import DeliverCheck from './DeliverCheck'
-import DeliverCheckBtn from '../../utils/DeliverCheckBtn'
-// Data
-import es from 'date-fns/locale/es'
-import { createValues, createValuesSchema } from './initialValues'
+import Table from '../../utils/tables'
 
-registerLocale('es', es)
-setDefaultLocale('es')
-
-const initialValues = {
-	...createValues
-}
-const validationSchema = Yup.object({
-	...createValuesSchema
-})
-
-const ChecksToDeliverTable = () => {
-	const columns = React.useMemo(
-		() => [
-			{
-				Header: () => <div className='w-44'>Número de Cheque</div>,
-				accessor: 'checkNumber'
-			},
-			{
-				Header: () => <div className='w-36'>Fecha</div>,
-				accessor: 'date',
-				Cell: ({ value }) => {
-					const [startDate, setStartDate] = useState(value)
-
-					return (
-						<DatePicker
-							readOnly={true}
-							locale='es'
-							selected={startDate}
-							onChange={(date) => setStartDate(date)}
-							className='table-field'
-						/>
-					)
-				}
-			},
-			{
-				Header: () => <div className='w-72'>Páguese a</div>,
-				accessor: 'payTo'
-			},
-			{
-				Header: () => <div className='w-36'>Moneda</div>,
-				accessor: 'currency',
-				width: 800
-			},
-			{
-				Header: () => <div className='w-36'>Monto</div>,
-				accessor: 'amount'
-			},
-			{
-				Header: '',
-				accessor: 'deliver',
-				Cell: (props) => {
-					const row = props.row.index
-
-					const rowData = props.data[row]
-					const [open, setOpen] = useState(false)
-					const closeModal = () => setOpen(false)
-
-					return (
-						<Popup
-							trigger={
-								<button
-									type='button'
-									className='btn bg-blue-blue btn-border-blue flex items-center gap-2 col-span-2 min-w-min'
-								>
-									<FaPaperPlane />
-								</button>
-							}
-							modal
-							nested
-							onClose={closeModal}
-						>
-							{(close) => (
-								<div className='section'>
-									<Formik
-										initialValues={initialValues}
-										validationSchema={validationSchema}
-										onSubmit={(values) => {
-											alert(JSON.stringify(values, null, 2))
-										}}
-									></Formik>
-
-									<DeliverCheck />
-									<DeliverCheckBtn onClick={close} />
-								</div>
-							)}
-						</Popup>
-					)
-				}
-			}
-		],
-		[]
-	)
-	const rows = React.useMemo(
-		() => [
-			{
-				// date must be set in english format, it will be displayed in spanish
-				currency: 'Euro',
-				date: new Date('12/20/2020'),
-				checkNumber: '901332',
-				amount: '3,543.84',
-				payTo: 'Juan Bosco Matus Gutiérrez'
-			},
-
-			{
-				currency: 'Dolar',
-				date: new Date(),
-				checkNumber: '302312',
-				amount: '4323.31',
-				payTo: 'Carlos Eduardo Castillo Pereira'
-			},
-
-			{
-				currency: 'Dolar',
-				date: new Date(),
-				checkNumber: '380212',
-				amount: '3312.32',
-				payTo: 'Carlos Fernando Arcia Castro'
-			},
-			{
-				currency: 'Dolar',
-				date: new Date(),
-				checkNumber: '381312',
-				amount: '100.00',
-				payTo: 'El Gato de Niudot'
-			}
-		],
-
-		[]
-	)
-
-	const [data, setData] = React.useState(rows)
-	const [originalData] = React.useState(data)
-	const [skipPageReset, setSkipPageReset] = React.useState(false)
-
-	// We need to keep the table from resetting the pageIndex when we
-	// Update data. So we can keep track of that flag with a ref.
-
-	// When our cell renderer calls updateMyData, we'll use
-	// the rowIndex, columnId and new value to update the
-	// original data
-	const updateMyData = (rowIndex, columnId, value) => {
-		// We also turn on the flag to not reset the page
-		setData((old) =>
-			old.map((row, index) => {
-				if (index === rowIndex) {
-					return {
-						...old[rowIndex],
-						[columnId]: value
-					}
-				}
-				return row
-			})
-		)
-	}
-
-	React.useEffect(() => {
-		setSkipPageReset(false)
-	}, [data])
-	console.log(data)
+const ChecksToDeliverTable = ({ togglePopup }) => {
+	const headers = ['No. Cheque', 'Fecha', 'Paguese a', 'Moneda', 'Monto']
+	const rows = [
+		{
+			number: 69,
+			date: '10/09/2001',
+			to: 'Juan Matus',
+			currency: 'Cordoba',
+			value: 69.69
+		},
+		{
+			number: 128,
+			date: '10/09/2001',
+			to: 'Juan Matus',
+			currency: 'Cordoba',
+			value: 69.69
+		},
+		{
+			number: 256,
+			date: '10/09/2001',
+			to: 'Juan Matus',
+			currency: 'Cordoba',
+			value: 69.69
+		},
+		{
+			number: 512,
+			date: '10/09/2001',
+			to: 'Juan Matus',
+			currency: 'Cordoba',
+			value: 69.69
+		},
+		{
+			number: 69,
+			date: '10/09/2001',
+			to: 'Juan Matus',
+			currency: 'Cordoba',
+			value: 69.69
+		}
+	]
 
 	return (
 		<>
-			<Table columns={columns} data={data} updateMyData={updateMyData} />
-			<div className='mt-4 flex gap-2 pb-4 flex-wrap'>
-				<button
-					type='button'
-					className='btn bg-blue-blue btn-border-blue inline-flex items-center gap-2 sm:break-words sm:text-sm sm:min-w-full overflow-visible sm:mb-3'
-				>
-					<FaPrint className='align-middle sm:mr-2' />
-					<span>Listado de Cheques Por Entregar</span>
-				</button>
-				<button
-					type='button'
-					className='btn bg-gray-cstm-14 inline-flex items-center gap-2 sm:break-words sm:text-sm sm:min-w-full overflow-hidden col-auto '
-				>
-					<FaPrint className='align-middle sm:mr-2' />
-					Reporte de Cheques
-				</button>
-			</div>
+			<Table headers={headers} rows={rows} />
 		</>
 	)
 }
--- niudot-app/src/components/pages/checks/to_deliver/index.js
+++ niudot-app/src/components/pages/checks/to_deliver/index.js
@@ -1,18 +1,67 @@
 // React and Router Stuff
-import React, { useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 // Other Components
-import ChecksToDeliverCreate from './ChecksToDeliverCreate'
+import SearchCheckForm from '../../utils/search/SearchCheckForm'
 // Context
 import routesContext from '../../../../context/routes/routesContext'
+import ChecksToDeliverTable from './ChecksToDeliverTable'
+import DeliverPopup from './popups/DeliverPopup'
 
 const ChecksToDeliver = () => {
 	const { changePage } = useContext(routesContext)
+	const [showPopup, setShowPopup] = useState({
+		deliver: false,
+		delete: false,
+		null: false,
+		print: false,
+		print_report: false,
+		print_list: false
+	})
 
 	useEffect(() => {
 		changePage('Cheques Por Entregar')
 	}, [])
 
-	return <ChecksToDeliverCreate />
+	const togglePopup = (popup) => {
+		switch (popup) {
+			case 'deliver':
+				setShowPopup({ ...showPopup, deliver: !showPopup.deliver })
+				break
+		}
+	}
+
+	return (
+		<div className='section'>
+			<SearchCheckForm />
+			<ChecksToDeliverTable togglePopup={() => togglePopup('deliver')} />
+			{showPopup.deliver && (
+				<DeliverPopup togglePopup={() => togglePopup('deliver')} />
+			)}
+			<div className='my-4 flex gap-2 justify-center flex-wrap'>
+				<button
+					className='btn flex items-center bg-blue-blue btn-border-blue'
+					onClick={() => togglePopup('deliver')}
+				>
+					Entregar
+				</button>
+				<button className='btn flex items-center bg-blue-blue btn-border-blue'>
+					Borrar
+				</button>
+				<button className='btn flex items-center bg-blue-blue btn-border-blue'>
+					Anular
+				</button>
+				<button className='btn flex items-center bg-blue-blue btn-border-blue'>
+					Imprimir
+				</button>
+				<button className='btn flex items-center bg-blue-blue btn-border-blue'>
+					Imprimir Listado de Cheques
+				</button>
+				<button className='btn flex items-center bg-blue-blue btn-border-blue'>
+					Imprimir Reporte de Cheques
+				</button>
+			</div>
+		</div>
+	)
 }
 
 export default ChecksToDeliver
