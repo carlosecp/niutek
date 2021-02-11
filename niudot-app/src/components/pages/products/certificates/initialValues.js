8a770ca669a2fd49f9f8611a9325ea5806cfb1f2
satbh <carlosarcia1827@gmail.com>
2021-02-11 16:05:12 -0600

    validation schema certificates

--- niudot-app/src/components/pages/products/certificates/initialValues.js
+++ niudot-app/src/components/pages/products/certificates/initialValues.js
@@ -9,3 +9,31 @@ export const initialValues = {
 	p_capitaliza_meses: 0,
 	p_cod_documento: 1,
 }
+
+export const validationSchema = Yup.object({
+	p_nombre: Yup.string()
+		.max(50, 'Caracteres Máximos 50')
+		.required('Requerido'),
+	p_descripcion: Yup.string()
+		.max(50, 'Caracteres Máximos 50')
+		.required('Requerido'),
+	p_cod_moneda: Yup.mixed()
+		.notOneOf([0], 'Seleccione un valor')
+		.required('Requerido'),
+	p_tasa_interes: Yup.number('Valor Numérico')
+		.min(0, 'Valor Mínimo 0')
+		.required('Requerido'),
+	p_monto_minimo: Yup.number('Valor Numérico')
+		.min(0, 'Valor Mínimo 0')
+		.required('Requerido'),
+	p_monto_maximo: Yup.number('Valor Numérico')
+		.min(0, 'Valor Mínimo 0')
+		.required('Requerido'),
+	p_plazo_meses: Yup.number('Valor Numérico')
+		.min(0, 'Valor Mínimo 0')
+		.required('Requerido'),
+	p_capitaliza_meses: Yup.number('Valor Numérico')
+		.min(0, 'Valor Mínimo 0')
+		.required('Requerido'),
+	pdc_cod_documento: Yup.array().of(Yup.string()),
+})
