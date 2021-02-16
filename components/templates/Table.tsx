c0da8aa6e2af0dda6043bceab6162bc537dc6e39
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-02-16 13:49:11 -0300

    Return to working tables w/ Formik, before pagination, gonna do pagination tomorrow

--- components/templates/Table.tsx
+++ components/templates/Table.tsx
@@ -1,97 +1,64 @@
-import { useEffect, memo } from 'react'
-import { useTable, usePagination } from 'react-table'
+import { memo } from 'react'
+import { useTable } from 'react-table'
 
-const Table = ({ columns, data, fetchData, loading }) => {
+// A el componente general Table se le pasa un tipo de dato de cualquiera de las tables disponibles en toda la aplicacion
+const Table = ({ columns, data }) => {
 	const {
 		getTableProps,
 		getTableBodyProps,
 		headerGroups,
+		rows,
 		prepareRow,
-		page,
-		canPreviousPage,
-		canNextPage,
-		pageOptions,
-		pageCount,
-		nextPage,
-		previousPage,
-		setPageSize,
-		state: { pageIndex, pageSize },
-	} = useTable(
-		{
-			columns,
-			data,
-			initialState: { pageIndex: 0 },
-		},
-		usePagination
-	)
-
-	console.log(data)
+	} = useTable({
+		columns,
+		data,
+	})
 
 	return (
-		<div className='my-2 rounded-lg shadow'>
-			<div className='overflow-x-auto no-scrollbar'>
-				<div className='align-middle inline-block min-w-full'>
-					<div className='overflow-hidden border-b border-gray-200 rounded-t-lg'>
-						<table
-							{...getTableProps()}
-							className='min-w-full divide-y divide-gray-200'
+		<div className='overflow-x-auto'>
+			<div className='my-2 align-middle inline-block min-w-full sm:px-1'>
+				<div className='shadow overflow-hidden border-b border-gray-200 rounded-lg'>
+					<table
+						{...getTableProps()}
+						className='min-w-full divide-y divide-gray-200'
+					>
+						<thead className='bg-gray-50'>
+							{headerGroups.map((headerGroup) => (
+								<tr {...headerGroup.getHeaderGroupProps()}>
+									{headerGroup.headers.map((column) => (
+										<th
+											{...column.getHeaderProps()}
+											className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+										>
+											{column.render('Header')}
+										</th>
+									))}
+								</tr>
+							))}
+						</thead>
+						<tbody
+							{...getTableBodyProps()}
+							className='bg-white divide-y divide-gray-200'
 						>
-							<thead className='bg-gray-50'>
-								{headerGroups.map((headerGroup) => (
-									<tr {...headerGroup.getHeaderGroupProps()}>
-										{headerGroup.headers.map((column) => (
-											<th
-												{...column.getHeaderProps()}
-												className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
-											>
-												{column.render('Header')}
-											</th>
-										))}
+							{rows.map((row) => {
+								prepareRow(row)
+								return (
+									<tr {...row.getRowProps()}>
+										{row.cells.map((cell) => {
+											return (
+												<td
+													{...cell.getCellProps()}
+													className='pl-4 py-2 whitespace-nowrap'
+												>
+													{cell.render('Cell')}
+												</td>
+											)
+										})}
 									</tr>
-								))}
-							</thead>
-							<tbody
-								{...getTableBodyProps()}
-								className='bg-white divide-y divide-gray-200'
-							>
-								{page.map((row) => {
-									prepareRow(row)
-									return (
-										<tr {...row.getRowProps()}>
-											{row.cells.map((cell) => {
-												return (
-													<td
-														{...cell.getCellProps()}
-														className='pl-4 py-2 whitespace-nowrap'
-													>
-														{cell.render('Cell')}
-													</td>
-												)
-											})}
-										</tr>
-									)
-								})}
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
-			<div className='bg-white rounded-b-lg px-4 py-3 flex items-center justify-between'>
-				<div className='flex-1 flex justify-between'>
-					<button
-						type='button'
-						className='relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500'
-						onClick={() => previousPage()}
-					>
-						Anterior
-					</button>
-					<button
-						type='button'
-						className='ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500'
-						onClick={() => nextPage()}
-					>
-						Siguiente
-					</button>
+								)
+							})}
+						</tbody>
+					</table>
 				</div>
 			</div>
 		</div>
