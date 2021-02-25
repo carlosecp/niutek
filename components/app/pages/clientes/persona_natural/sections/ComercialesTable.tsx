9007555c93c22e5e351f631e224a0bed8da6362d
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-02-25 18:49:46 -0300

    Added button styles to comerciales table

--- components/app/pages/clientes/persona_natural/sections/ComercialesTable.tsx
+++ components/app/pages/clientes/persona_natural/sections/ComercialesTable.tsx
@@ -119,7 +119,7 @@ const ComercialesTable = <Data, RefSchema>(props: Props<RefSchema>) => {
 				</h1>
 				<button
 					type='button'
-					className='inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary btn-disabled transition'
+					className='btn btn-primary'
 					onClick={onAdd}
 					disabled={data.length >= props.limit}
 				>
