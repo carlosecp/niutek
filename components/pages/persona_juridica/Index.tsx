5a295e8513783c6e94189e04b27603ef6e11ba80
Juan Matus <juanmatusg16@gmail.com>
2021-03-13 12:51:32 -0600

    Cambios para el fantasma del castillo

--- components/pages/persona_juridica/Index.tsx
+++ components/pages/persona_juridica/Index.tsx
@@ -5,7 +5,6 @@ import type {
 	PersonaJuridicaSearchResult
 } from '@/data/persona_juridica'
 import * as React from 'react'
-import useIndex from '@/lib/useIndex'
 import {
 	initialValues,
 	navLinks,
@@ -18,94 +17,36 @@ import {
 	RefBancarias,
 	Proveedores
 } from './components'
-import { Navbar, Navigation, Search, Results, Alerts } from '@/layouts/index'
-import Form from '@/components/forms'
+import { Index } from '@/components/Index'
 
-interface Props {
-	options: {
-		tabla: TablaOptions
-		deptos_municipios: DeptosOption[]
-	}
+const config: Config<
+	PersonaJuridicaValues,
+	PersonaJuridicaValidationSchema,
+	PersonaJuridicaSearchResult
+> = {
+	pageType: 'cliente',
+	pageName: 'juridico',
+	navLinks,
+	getDescription,
+	initialValues
 }
 
-const Index = (props: Props) => {
-	const state = useIndex<PersonaJuridicaValues, PersonaJuridicaSearchResult>({
-		key: 'p_cod_cliente',
-		initialValues: initialValues.values,
-		url: {
-			fetch: 'datos_cliente_juridico',
-			write: 'cliente_juridico'
-		}
-	})
+const Wrapper = Index<
+	PersonaJuridicaValues,
+	PersonaJuridicaValidationSchema,
+	PersonaJuridicaSearchResult
+>(config)
 
-	const navbarProps = {
-		loading: state.loading,
-		title: 'Persona Jurídica',
-		onReset: () => state.setData(initialValues.values),
-		setEditingExisting: state.setEditingExisting,
-		toggleNavigation: () => state.setShowNavigation(!state.showNavigation)
-	}
-
-	const formProps = {
-		accessKey: 'p_cod_cliente',
-		currentId: state.currentId,
-		validations: initialValues.validations,
-		values: state.data,
-		writeData: state.writeData
-	}
-
-	const navigationProps = {
-		navLinks,
-		showNavigation: state.showNavigation,
-		toggleNavigation: () => state.setShowNavigation(!state.showNavigation)
-	}
-
-	const searchProps = {
-		config: {
-			placeholder: 'Buscar persona jurídica',
-			url: 'busca/clientes_juridico'
-		},
-		loading: state.loading,
-		setLoading: state.setLoading,
-		setSearchResults: state.setSearchResults
-	}
-
-	const resultsProps = {
-		loading: state.loading,
-		results: state.searchResults,
-		getData: state.getData,
-		getDescription,
-		setCurrentId: state.setCurrentId
-	}
-
+const pjIndex = (props: Props) => {
 	return (
-		<main className='sm:ml-64 relative bg-light'>
-			<Navbar {...navbarProps} />
-			<div className='flex flex-col py-4 lg:pr-64'>
-				<Alerts alerts={state.alerts} closeAlert={state.closeAlert} />
-				<Form<PersonaJuridicaValues, PersonaJuridicaValidationSchema>
-					{...formProps}
-				>
-					<PersonaJuridica options={props.options} />
-					<OrigenFondos />
-					<RefComerciales />
-					<RefBancarias options={props.options} />
-					<Proveedores options={props.options} />
-					<button
-						type='submit'
-						className='btn btn-primary'
-						disabled={state.loading}
-					>
-						Submit
-					</button>
-				</Form>
-				<Navigation {...navigationProps}>
-					<Search<PersonaJuridicaSearchResult> {...searchProps} />
-					<Results {...resultsProps} />
-				</Navigation>
-			</div>
-		</main>
+		<Wrapper {...props}>
+			<PersonaJuridica options={props.options} />
+			<OrigenFondos />
+			<RefComerciales />
+			<RefBancarias options={props.options} />
+			<Proveedores options={props.options} />
+		</Wrapper>
 	)
 }
 
-export default Index
+export default pjIndex
