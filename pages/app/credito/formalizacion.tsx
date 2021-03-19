9ede5a6a72ba6d79309f2a3c31abfbdbceef4ae0
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-03-19 14:31:19 -0300

    Dont know what I changed

--- pages/app/credito/formalizacion.tsx
+++ pages/app/credito/formalizacion.tsx
@@ -11,8 +11,6 @@ interface OptionsTypes {
 }
 
 const index = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
-	console.log(props)
-
 	const indexProps = {
 		options: props.options
 	}
@@ -38,8 +36,6 @@ export const getStaticProps = async () => {
 	}
 
 	const res = await axios.post(req.path, req.body, { headers: req.headers })
-	console.log(res.data)
-
 	return {
 		props: {
 			options: res.data.lee as OptionsTypes
--- pages/app/credito/resolucion.tsx
+++ pages/app/credito/resolucion.tsx
@@ -11,8 +11,6 @@ interface OptionsTypes {
 }
 
 const index = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
-	console.log(props)
-
 	const indexProps = {
 		options: props.options
 	}
@@ -38,7 +36,6 @@ export const getStaticProps = async () => {
 	}
 
 	const res = await axios.post(req.path, req.body, { headers: req.headers })
-	console.log(res.data)
 
 	return {
 		props: {
