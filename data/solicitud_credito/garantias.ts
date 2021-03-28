48bd80b699442a83e9e310013501dd509e25b0f5
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-03-28 17:02:10 -0300

    Added solicitud values

--- data/solicitud_credito/garantias.ts
+++ data/solicitud_credito/garantias.ts
@@ -1,91 +1,32 @@
 import * as yup from 'yup'
-import moment from 'moment'
 
 export const refSchema = {
 	prc_cod_moneda: 0,
-	prc_cod_tipo: 0,
 	prc_descripcion_cargo: '',
-	prc_valor: 0
+	prc_valor: 0,
+	prc_cod_tipo: 0
 }
 
 export type RefSchema = typeof refSchema
 
-const values: RefSchema[] = [
+const values: RefSchema[] = []
+
+const testingValues = [
 	{
+		prc_cod_moneda: 0,
 		prc_cod_tipo: 0,
-		prc_descripcion_cargo: '',
-		prc_valor: 0,
-		prc_cod_moneda: 0
+		prc_valor: (0).toFixed(2),
+		prc_descripcion_cargo: 'prc_descripcion_cargo 0'
+	},
+	{
+		prc_cod_moneda: 0,
+		prc_cod_tipo: 0,
+		prc_valor: (0).toFixed(2),
+		prc_descripcion_cargo: 'prc_descripcion_cargo 1'
 	}
 ]
 
-const validations = {
-	p_nombres: yup
-		.string()
-		.required('Requerido')
-		.max(50, 'Caracteres máximos 50'),
-	p_apellidos: yup
-		.string()
-		.required('Requerido')
-		.max(50, 'Caracteres máximos 50'),
-	p_sexo: yup
-		.mixed()
-		.notOneOf([0], 'Seleccione un valor')
-		.required('Requerido'),
-	p_tipo_doc: yup
-		.mixed()
-		.notOneOf([0], 'Seleccione un valor')
-		.required('Requerido'),
-	p_num_doc: yup
-		.string()
-		.required('Requerido')
-		.max(30, 'Caracteres máximos 30'),
-	p_cod_nac: yup
-		.mixed()
-		.notOneOf([0], 'Seleccione un valor')
-		.required('Requerido'),
-	p_cod_depto: yup
-		.mixed()
-		.notOneOf([0], 'Seleccione un valor')
-		.required('Requerido'),
-	p_cod_muni: yup
-		.mixed()
-		.notOneOf([0], 'Seleccione un valor')
-		.required('Requerido'),
-	p_direccion: yup
-		.string()
-		.required('Requerido')
-		.max(100, 'Caracteres máximos 100'),
-	p_notas: yup
-		.string()
-		.required('Requerido')
-		.max(300, 'Caracteres máximos 300'),
-	p_telefono1: yup
-		.string()
-		.required('Requerido')
-		.max(30, 'Caracteres máximos 30'),
-	p_telefono2: yup
-		.string()
-		.required('Requerido')
-		.max(30, 'Caracteres máximos 30'),
-	p_lugar_nacimiento: yup
-		.string()
-		.required('Requerido')
-		.max(30, 'Caracteres máximos 30'),
-	p_fecha_nacimiento: yup
-		.date()
-		.required('Requerido')
-		.max(moment(), 'Fecha inválida')
-		.typeError('Fecha Inválida (formato YYYY-MM-DD)'),
-	p_cargo_publico: yup
-		.mixed()
-		.notOneOf([0], 'Seleccione un valor')
-		.required('Requerido'),
-	p_num_hijos: yup
-		.number()
-		.integer('Valor entero')
-		.min(0, 'Valor mínimo 0')
-		.required('Requerido')
-}
+const validations = {}
 
-export const garantias = { values, validations }
+// export const garantias = { values, validations }
+export const garantias = { values: testingValues, validations }
