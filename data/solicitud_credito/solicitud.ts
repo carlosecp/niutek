d26796cad830e567373dc33541e06962e807a54f
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-03-28 16:55:04 -0300

    Fixed solicitud initialvalues not showing

--- data/solicitud_credito/solicitud.ts
+++ data/solicitud_credito/solicitud.ts
@@ -17,23 +17,21 @@ const values = {
 	pga_cod_garantia: []
 }
 
-const testingValues = [
-	{
-		p_cod_promotor: 0,
-		p_fecha_solicitud: moment().format('YYYY-MM-DD'),
-		p_cod_act_economica: 0,
-		p_monto_solicitado: 0,
-		p_cod_sec_economico: 0,
-		p_cod_moneda: 0,
-		p_cod_destino_credito: 0,
-		p_tasa_interes: 0,
-		p_plazo: 0,
-		p_cod_frec_plazo: 0,
-		p_cod_frecuencia_pago: 0,
-		pdc_cod_documento: ['2', '1'],
-		pga_cod_garantia: ['2', '1']
-	}
-]
+const testingValues = {
+	p_cod_promotor: 0,
+	p_fecha_solicitud: moment().format('YYYY-MM-DD'),
+	p_cod_act_economica: 0,
+	p_monto_solicitado: 0,
+	p_cod_sec_economico: 0,
+	p_cod_moneda: 0,
+	p_cod_destino_credito: 0,
+	p_tasa_interes: 0,
+	p_plazo: 0,
+	p_cod_frec_plazo: 0,
+	p_cod_frecuencia_pago: 0,
+	pdc_cod_documento: ['2', '1'],
+	pga_cod_garantia: ['2', '1']
+}
 
 const validations = {}
 // const validations = {
