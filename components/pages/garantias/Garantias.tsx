049db1c77eed3c806f26c4189e739e64fbb1f6e4
Carlos Castillo <47466248+itscarlosecp@users.noreply.github.com>
2021-04-04 21:37:56 -0300

    Dont know what I changed

--- components/pages/garantias/Garantias.tsx
+++ components/pages/garantias/Garantias.tsx
@@ -1,83 +1,10 @@
-import type { AuthStateType } from '@/context/auth/authContext'
 import * as React from 'react'
-import axios from 'axios'
-import authContext from '@/context/auth/authContext'
 import { navLinks, NavLinks } from '@/data/garantias'
 import { Text, Select, TextArea } from '@/components/forms'
 
 const SECTION_NAME = NavLinks.Garantias
 
-type CurrentId = string | number | null
-
-const getGarantias = async (
-	auth: AuthStateType,
-	setLoading: (x: boolean) => void,
-	currentId: CurrentId
-) => {
-	const req = {
-		path: `${process.env.backend}/proc/lee/lista_garantias_cliente`,
-		body: {
-			p_cod_empresa: auth.user.p_cod_empresa,
-			p_cod_sucursal: auth.user.p_cod_sucursal,
-			p_cod_cliente: currentId
-		},
-		headers: {
-			'Content-Type': 'application/json',
-			'Access-Control-Allow-Credentials': 'true'
-		}
-	}
-
-	console.group(
-		'%cGetting Data',
-		'font-weight: bold; font-size: 16px; text-transform: uppercase; text-decoration: underline'
-	)
-	console.log(
-		'%c config ',
-		'background: #06B6D4; color: #FFFFFF; font-weight: bold',
-		req
-	)
-	setLoading(true)
-
-	try {
-		const res = await axios.post(req.path, req.body, {
-			headers: req.headers
-		})
-
-		return res.data as number[]
-	} catch (err) {
-		return []
-	} finally {
-		console.groupEnd()
-		setLoading(false)
-	}
-}
-
-interface Props {
-	currentId: CurrentId
-	loading: boolean
-	setLoading: (x: boolean) => void
-}
-
-const Garantias = (props: Props) => {
-	const [garantias, setGarantias] = React.useState<number[]>()
-
-	const auth = React.useContext(authContext)
-
-	const getInfo = async () => {
-		const garantiasData = await getGarantias(
-			auth,
-			props.setLoading,
-			props.currentId
-		)
-		setGarantias(garantiasData)
-	}
-
-	React.useEffect(() => {
-		if (props.currentId) {
-			getInfo()
-		}
-	}, [props.currentId])
-
+const Garantias = () => {
 	return (
 		<section id={navLinks[SECTION_NAME].anchor}>
 			<a
@@ -88,30 +15,24 @@ const Garantias = (props: Props) => {
 			<h1 className='font-medium text-xl text-gray-900'>
 				{navLinks[SECTION_NAME].name}
 			</h1>
-			<article className='form-section my-2 flex justify-between items-center'>
-				<div className='text-sm font-medium text-gray-700'>
-					{props.currentId
-						? `Cliente seleccionado: ${props.currentId}`
-						: 'Ningun cliente seleccionado'}
-				</div>
-			</article>
+			<article className='form-section my-2 flex justify-between items-center'></article>
 			<article className='form-section my-2 grid grid-cols-12 gap-4'>
 				<Select
 					name='p_cod_gar'
 					label='Seleccionar garantía'
 					classes={{
-						container: 'fc-lg md:fc break-line',
+						container: 'fc-lg md:fc',
 						input: 'w-full block form-input form-input-border'
 					}}
 				>
-					{garantias &&
-						garantias.map((garantia) => <option value={1}>Opcion 1</option>)}
+					<option value={1}>Opcion 1</option>
+					<option value={2}>Opcion 2</option>
 				</Select>
 				<Select
 					name='p_cod_gar'
 					label='Tipo de garantía'
 					classes={{
-						container: 'fc-lg md:fc break-line',
+						container: 'fc-lg md:fc',
 						input: 'w-full block form-input form-input-border'
 					}}
 				>
@@ -122,7 +43,7 @@ const Garantias = (props: Props) => {
 					name='p_cod_cla'
 					label='Clasificación'
 					classes={{
-						container: 'fc-lg md:fc break-line',
+						container: 'fc-lg md:fc',
 						input: 'w-full block form-input form-input-border'
 					}}
 				>
@@ -133,7 +54,7 @@ const Garantias = (props: Props) => {
 					name='p_cod_cat'
 					label='Categoría'
 					classes={{
-						container: 'fc-lg md:fc break-line',
+						container: 'fc-lg md:fc',
 						input: 'w-full block form-input form-input-border'
 					}}
 				>
